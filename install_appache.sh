4ea51bd87735d27995e9769849a78b54782be9fd
okakpuolivia <oliviaokakpu@gmail.com>
2024-10-19 13:03:04 +0100

    my first commit

---
+++ install_appache.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "Installing Apache #############"
+sudo yum update -y
+sudo yum install httpd -y
+sudo service httpd start
+sudo service httpd status
+echo "Hello World from $(hostname -f)" > /var/www/html/index.html
